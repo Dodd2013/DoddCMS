992912da6e6707f6772bb1fed5c6ecd78e35388f
Dodd <291106637@qq.com>
2017-02-26 22:02:35 +0800

    完成登录功能
    Signed-off-by: Dodd <291106637@qq.com>

--- Controller/userCtrl.js
+++ Controller/userCtrl.js
@@ -1,10 +1,35 @@
 var User = require("../Model/user");
 var CryptoJS = require("crypto-js");
-var config=require("../config.js");
+var config = require("../config.js");
 var userCtrl = {
-
-	loginWithUNAndPWD: function({userName,password}}) {
-		User.findOne({where:{userName:userName,passWord:CryptoJS.AES.encrypt(password, config.secretKey);}})
+	//返回promise对象
+	loginWithUNAndPWD: function({
+		userName,
+		password
+	}) {
+		password=CryptoJS.MD5(password+config.secretKey).toString();
+		console.log(password);
+		var res={status: true};
+		return User.findOne({
+			where: {
+				userName: userName,
+			}
+		}).then(function(user) {
+			if (user && user.passWord == password) {
+				
+			} else if(user){
+				res={
+					msg: "PassWord is not right!",
+					status: false
+				};
+			}else{
+				res={
+					msg: "User is not exist!",
+					status: false
+				};
+			}
+			return res;
+		})
 	}
 }
 module.exports = userCtrl
--- Route/login.js
+++ Route/login.js
@@ -1,23 +1,33 @@
 var express = require('express');
 var router = express.Router();
-
+var userCtrl = require("../Controller/userCtrl");
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	res.jsonp({
-		msg: 'Error! Plase post your user name and password!',
+		msg: 'Error! Plase post your userName and passWord!',
 		status: "no"
 	});
 });
 router.post('/', function(req, res, next) {
-	if (req.body.username == undefined || req.body.password == undefined){
+	if (req.body.userName == undefined || req.body.passWord == undefined) {
 		res.jsonp({
-			msg: 'Error! Plase post your user name and password!',
+			msg: 'Error! Plase post your userName and passWord!',
 			status: "no"
 		});
-	}else{
-		res.jsonp({
-			msg: `Login with ${req.body.username}`,
-			status: "ok"
+	} else {
+		userCtrl.loginWithUNAndPWD(req.body).then(function(data) {
+			if (data.status) {
+				req.session.userName=req.body.userName;
+				res.jsonp({
+					msg: `Login with ${req.body.userName}`,
+					status: "ok"
+				});
+			} else {
+				res.jsonp({
+					msg: data.msg,
+					status: "no"
+				});
+			}
 		});
 	}
 });
