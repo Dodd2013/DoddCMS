b9bcaeb9320992b8f8aa2bfebedd2b91c82ab0a1
Dodd <291106637@qq.com>
2017-03-13 21:49:16 +0800

    cxz

--- html/config.js
+++ html/config.js
@@ -4,7 +4,7 @@ var config = {
 };
 var adminPages = {
 	"1": {
-		"icon":"fa-user-o",
+		"icon": "fa-user-o",
 		"name": "用户管理"
 	},
 	"101": {
@@ -15,54 +15,58 @@ var adminPages = {
 		"url": "#",
 		"name": "用户角色管理"
 	},
-	"2":{
-		"icon":"fa-database",
-		"name":"系统设置"
+	"2": {
+		"icon": "fa-database",
+		"name": "系统设置"
 	},
-	"201":{
+	"201": {
 		"url": "#",
 		"name": "导航栏管理"
 	},
-	"202":{
-		"url":"#",
-		"name":"广告位管理"
+	"202": {
+		"url": "#",
+		"name": "广告位管理"
 	},
-	"203":{
-		"url":"#",
-		"name":"数据库备份"
+	"203": {
+		"url": "#",
+		"name": "数据库备份"
 	},
-	"3":{
-		"icon":"fa-superpowers",
-		"name":"权限管理"
+	"3": {
+		"icon": "fa-superpowers",
+		"name": "权限管理"
 	},
-	"301":{
-		"url":"#",
-		"name":"角色管理"
+	"301": {
+		"url": "#",
+		"name": "角色管理"
 	},
-	"302":{
-		"url":"#",
-		"name":"角色权限管理"
+	"302": {
+		"url": "#",
+		"name": "角色权限管理"
 	},
-	"4":{
-		"icon":"fa-book",
-		"name":"内容管理"
+	"4": {
+		"icon": "fa-book",
+		"name": "内容管理"
 	},
-	"401":{
-		"url":"#",
-		"name":"栏目管理"
+	"401": {
+		"url": "#",
+		"name": "栏目管理"
 	},
-	"402":{
-		"url":"#",
-		"name":"投稿"
+	"402": {
+		"url": "#",
+		"name": "投稿"
 	},
-	"403":{
-		"url":"#",
-		"name":"内容管理"
+	"403": {
+		"url": "#",
+		"name": "内容管理"
 	}
 };
-$.ajaxSetup({
-	xhrFields: {
-		withCredentials: true //全局跨域session
-	}
-});
-if(PNotify)PNotify.prototype.options.styling = "bootstrap3"; //"fontawesome";
+if (typeof($) != "undefined"){
+	$.ajaxSetup({
+		xhrFields: {
+			withCredentials: true //全局跨域session
+		}
+	});
+}
+if (typeof(PNotify) != "undefined") {
+	PNotify.prototype.options.styling = "bootstrap3"; //"fontawesome";}
+}
