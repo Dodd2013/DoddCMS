f42dbfbf4299a39ad64eaf2af8a0dac7f78767d3
Dodd <291106637@qq.com>
2017-03-16 16:17:02 +0800

    todo，登录有问题，admin的重构出现问题

--- html/js/view/admin/index.js
+++ html/js/view/admin/index.js
@@ -245,94 +245,121 @@
 // 				.otherwise('/');
 // 		}
 // 	]);
-// 	$("#loginSubmit").on('click', "", function(event) {
-// 		var $scope = $("html").scope(); //获取angular的scope
-// 		$.ajax({
-// 			url: config.api + '/login',
-// 			type: 'POST',
-// 			dataType: 'json',
-// 			data: {
-// 				userName: $scope.userName,
-// 				passWord: $scope.password
-// 			}
-// 		}).success(function(data) {
-// 			if (data.status === "ok") window.location.reload();
-// 			else {
-// 				var msg;
-// 				if (data.msg == "PassWord is not right!") msg = "密码不正确！";
-// 				else msg = "用户不存在！";
-// 				new PNotify({
-// 					type: 'error',
-// 					text: msg
-// 				});
-// 			}
-// 		});
-
+// $("#loginSubmit").on('click', "", function(event) {
+// 	var $scope = $("html").scope(); //获取angular的scope
+// 	$.ajax({
+// 		url: config.api + '/login',
+// 		type: 'POST',
+// 		dataType: 'json',
+// 		data: {
+// 			userName: $scope.userName,
+// 			passWord: $scope.password
+// 		}
+// 	}).success(function(data) {
+// 		if (data.status === "ok") window.location.reload();
+// 		else {
+// 			var msg;
+// 			if (data.msg == "PassWord is not right!") msg = "密码不正确！";
+// 			else msg = "用户不存在！";
+// 			new PNotify({
+// 				type: 'error',
+// 				text: msg
+// 			});
+// 		}
 // 	});
+
+// });
 // 	$('.sidebar').on('click', '.nav-model .nav-model-header', function(event) {
 // 		$(event.target).parent(".nav-model").toggleClass('active');
 // 	});
-$.ajax({
-	url: config.api + '/login/status',
-	type: 'GET',
-	dataType: 'json',
-	data: {}
-}).success(function (data) {
-	var $scope = $("html").scope(); //获取angular的scope
-	if (data.isLogin) {
-		$scope.$apply(function () {
-			$scope.userName = data.userName;
-		});
-		removeLoadCover();
-	} else {
-		$('#loginModal').modal({
-			backdrop: 'static',
-			keyboard: false
-		});
-	}
-});
+// $.ajax({
+// 	url: config.api + '/login/status',
+// 	type: 'GET',
+// 	dataType: 'json',
+// 	data: {}
+// }).success(function(data) {
+// 	var $scope = $("html").scope(); //获取angular的scope
+// 	if (data.isLogin) {
+// 		$scope.$apply(function() {
+// 			$scope.userName = data.userName;
+// 		});
+// 		removeLoadCover();
+// 	} else {
+// 		$('#loginModal').modal({
+// 			backdrop: 'static',
+// 			keyboard: false
+// 		});
+// 	}
+// });
 // 	//setTimeout(removeLoadCover, 2000);
 // })();
 // 
-define(['angular'], function (ng) {
-	require(['require'], function (require) {
-		var $ = require('jquery');
-		var config = require('config').config;
-		$('.sidebar').on('click', '.nav-model .nav-model-header', function (event) {
-			$(event.target).parent(".nav-model").toggleClass('active');
-		});
-		var app = angular.module('admin', ['ui.router']);
-		app.controller('adminCtrl', function ($scope, $http) {
-			$scope.config = config;
-			$scope.title = "管理主页-" + config.projectName;
-			$scope.userName = "";
-			$scope.password = "";
-			$scope.adminPages = adminPages;
-			$scope.navList = [{
-				"title": "4",
-				"nav": ["401", "402", "403"]
-			}, {
-				"title": "1",
-				"nav": ["101", "102"]
-			}, {
-				"title": "3",
-				"nav": ["301", "302"]
-			}, {
-				"title": "2",
-				"nav": ["201", "202", "203"]
-			}];
-		});
+define(['angular', 'require', 'jquery', 'config', 'bootstrap'], function (ng, require) {
+	var $ = require('jquery');
+	var config = require('config').config;
+	var adminPages = require('config').adminPages;
+	//todo 用ng去代替
+	$('.sidebar').on('click', '.nav-model .nav-model-header', function (event) {
+		$(event.target).parent(".nav-model").toggleClass('active');
+	});
+	//'ui.router'
+	var app = ng.module('admin', []);
+	app.controller('adminCtrl', function ($scope, $http) {
+		$scope.config = config;
+		$scope.title = "管理主页-" + config.projectName;
+		$scope.userName = "";
+		$scope.password = "";
+		$scope.adminPages = adminPages;
+		$scope.login = function () {
+			$http({
+				url: config.api + '/login',
+				method: 'POST',
+				xhrFields: {
+					withCredentials: true //全局跨域session
+
+				},
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded;'
+				},
+				transformRequest: $.param,
+				data: {
+					userName: $scope.userName,
+					passWord: $scope.password
+				}
+			}).then(function (data) {
+				if (data.status === "ok") window.location.reload();else {
+					var msg;
+					if (data.msg == "PassWord is not right!") msg = "密码不正确！";else msg = "用户不存在！";
+					alert(msg);
+					// new PNotify({
+					// 	type: 'error',
+					// 	text: msg
+					// });
+				}
+			});
+		};
+		$scope.navList = [{
+			"title": "4",
+			"nav": ["401", "402", "403"]
+		}, {
+			"title": "1",
+			"nav": ["101", "102"]
+		}, {
+			"title": "3",
+			"nav": ["301", "302"]
+		}, {
+			"title": "2",
+			"nav": ["201", "202", "203"]
+		}];
 		$http({
 			url: config.api + '/login/status',
-			type: 'GET',
-			dataType: 'json',
-			data: {}
-		}).success(function (data) {
-			var $scope = $("html").scope(); //获取angular的scope
+			method: 'GET',
+			xhrFields: {
+				withCredentials: true //全局跨域session
+			}
+		}).then(function (data) {
 			if (data.isLogin) {
-				$scope.$apply(function () {
-					$scope.userName = data.userName;
-				});
+				$scope.userName = data.userName;
 				removeLoadCover();
 			} else {
 				$('#loginModal').modal({
@@ -341,33 +368,33 @@ define(['angular'], function (ng) {
 				});
 			}
 		});
-		$.ajax({
-			url: config.api + '/login/status',
-			type: 'GET',
-			dataType: 'json',
-			data: {}
-		}).success(function (data) {
-			var $scope = $("html").scope(); //获取angular的scope
-			if (data.isLogin) {
-				$scope.$apply(function () {
-					$scope.userName = data.userName;
-				});
-				removeLoadCover();
-			} else {
-				$('#loginModal').modal({
-					backdrop: 'static',
-					keyboard: false
-				});
-			}
-		});
-
-		// ng.module('index', [])
-		// 	.config(function($httpProvider) {
-		// 		$httpProvider.defaults.withCredentials = true;
-		// 	})
-		// 	.controller('headCtrl', headCtrl)
-		// 	.controller('navCtrl', navCtrl)
-		// 	.controller('swiperCtrl', swiperCtrl);
-		// ng.bootstrap(document, ['index']);
 	});
+	ng.bootstrap(document, ['admin']);
+	// $.ajax({
+	// 	url: config.api + '/login/status',
+	// 	type: 'GET',
+	// 	dataType: 'json',
+	// 	data: {}
+	// }).success(function(data) {
+	// 	var $scope = $("html").scope(); //获取angular的scope
+	// 	if (data.isLogin) {
+	// 		$scope.$apply(function() {
+	// 			$scope.userName = data.userName;
+	// 		});
+	// 		removeLoadCover();
+	// 	} else {
+	// 		$('#loginModal').modal({
+	// 			backdrop: 'static',
+	// 			keyboard: false
+	// 		});
+	// 	}
+	// });
+
+	// ng.module('index', [])
+	// 	.config(function($httpProvider) {
+	// 		$httpProvider.defaults.withCredentials = true;
+	// 	})
+	// 	.controller('headCtrl', headCtrl)
+	// 	.controller('navCtrl', navCtrl)
+	// 	.controller('swiperCtrl', swiperCtrl);
 });
