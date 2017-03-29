f710c3169f80261804b504da0abd5ec557c377ef
Dodd <291106637@qq.com>
2017-03-29 10:18:22 +0800

    退出登录

--- html/js/view/admin/logout.js
+++ html/js/view/admin/logout.js
@@ -1,27 +1,47 @@
 'use strict';
 
-(function () {
+define(function (require, exports, module) {
+	var config = require('config').config;
+	var angualr = require('angular');
 	var app = angular.module('logout', []);
 	app.controller('logoutCtrl', function ($scope, $http) {
 		$scope.config = config;
 		$scope.title = "退出-" + config.projectName;
-	});
-	$.ajax({
-		url: config.api + '/logout',
-		type: 'GET',
-		dataType: 'json',
-		data: {}
-	}).success(function (data) {
-		if (data.logout) {
+		$http({
+			url: config.api + '/logout',
+			method: 'GET',
+			withCredentials: true,
+			headers: {
+				'Accept': "*/*"
+			}
+		}).then(function (data) {
 			alert("退出成功");
 			window.location.href = "/";
-			//TODO
-		} else {
-			alert("你没有登录，退出个P啊！");
-			window.location.href = "/admin";
-		}
-	}).fail(function () {
-		alert("退出失败,请稍后再试！");
-		//TODO
+		}, function (data) {
+			alert("退出失败,请稍后再试！");
+		});
 	});
-})();
+});
+// (function() {
+
+// 	$.ajax({
+// 		url: config.api + '/logout',
+// 		type: 'GET',
+// 		dataType: 'json',
+// 		data: {}
+// 	}).success(function(data) {
+// 		if (data.logout) {
+// 			alert("退出成功");
+// 			window.location.href = "/"
+// 				//TODO
+// 		} else {
+// 			alert("你没有登录，退出个P啊！");
+// 			window.location.href = "/admin"
+// 		}
+
+// 	}).fail(function() {
+// 		alert("退出失败,请稍后再试！");
+// 		//TODO
+// 	});
+
+// })();
